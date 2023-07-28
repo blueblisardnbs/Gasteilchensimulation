905e9463ed9e9ac4b38cc5adbc6e54660b70359e
Niel Bodamer <60315174+blueblisardnbs@users.noreply.github.com>
2023-07-28 18:26:54 +0200

    Update simulation.py
    Anfangspositionen werden nun so gewählt, dass sich Atome/ Teilchen nicht überlappen.

--- simulation.py
+++ simulation.py
@@ -1,29 +1,47 @@
 import numpy as np
 import scipy.spatial.distance as distance
-import math
 
-groesse_zeitschritt = 0.0005
+groesse_zeitschritt = 0.005
 
-anzahl_zeitschritte = 100000
-anzahl_teilchen = 50
+anzahl_zeitschritte = 10000
+anzahl_teilchen = 5000
 anzahl_dimensionen = 2
+
 m_1 = 1
 m_2 = 1
 
-radius_teilchen = 0.005
+radius_teilchen = 0.05
 
+seitenlänge_Käfig = 100
 
-def abstand(teilchen1, teilchen2):
+np.savetxt("Konfiguration.txt", (anzahl_teilchen, anzahl_zeitschritte))
+
+def generate_random_initial_pos(num_particles, num_dimensions, radius_particles):
+
+    box_left_wall  = [0.0,]*num_dimensions
+    box_right_wall = [seitenlänge_Käfig,]*num_dimensions
     
-    abstand_dimension = [0 for i in range(anzahl_dimensionen)]
-    for d in range(anzahl_dimensionen):
-        abstand_dimension[d] += orte[schritt-1, teilchen1, d]
-        abstand_dimension[d] -= orte[schritt-1, teilchen2, d]
+    pos = np.zeros([num_particles, num_dimensions])
         
-        abstand_dimension[d] = pow(abstand_dimension[d], 2)
+    md = 2.5 * radius_particles
     
-    return math.sqrt(sum(abstand_dimension))
+    coord_arrays = [ ]
+    for d in range(num_dimensions):
+        l = box_left_wall[d] + md
+        r = box_right_wall[d] - md
+        num_sites = min( int((r - l) / md), 100)
+        
+        coord_arrays.append( np.linspace(l, r, num_sites) )
     
+    coords = np.meshgrid(*coord_arrays)
+    n = coords[0].size
+    indices = np.random.choice(n, num_particles, replace=False)
+    
+    for d in range(num_dimensions):
+        pos[:,d] = np.ravel(coords[d])[indices]
+                
+    return pos
+
 def stoß(x, v, teilchen1, teilchen2):
     
     v_1 = v[teilchen1, :]
@@ -59,7 +77,7 @@ def zeitschritt(x, v, dt):
     v_neu = np.copy(v)
     for t in range(anzahl_teilchen):
         for d in range(anzahl_dimensionen):
-            if x[t, d] > 1 or x[t, d] < 0:
+            if x[t, d] > seitenlänge_Käfig or x[t, d] < 0:
                 v_neu[t,d] *= -1
                 
     distanzen = distance.pdist(x)
@@ -75,7 +93,10 @@ def zeitschritt(x, v, dt):
 
     return x_neu, v_neu
 
-anfangspositionen = np.random.random((anzahl_teilchen, anzahl_dimensionen))
+anfangspositionen = generate_random_initial_pos(anzahl_teilchen,anzahl_dimensionen, radius_teilchen)
+
+distanzen = distance.pdist(anfangspositionen)
+
 
 for position1 in range(len(anfangspositionen)):
     for position2 in range(len(anfangspositionen)):
@@ -83,10 +104,9 @@ for position1 in range(len(anfangspositionen)):
             continue
         while np.linalg.norm(anfangspositionen[position1] - anfangspositionen[position2]) < 2*radius_teilchen:
             anfangspositionen[position1] = 0.9*np.random.random(anzahl_dimensionen)
-        print(np.linalg.norm(anfangspositionen[position1] - anfangspositionen[position2]))
 
 anfangsgeschwindigkeiten = 2*np.random.random((anzahl_teilchen, anzahl_dimensionen))-1
-orte = 1.8*np.zeros([anzahl_zeitschritte, anzahl_teilchen, anzahl_dimensionen])-0.9
+orte = np.zeros([anzahl_zeitschritte, anzahl_teilchen, anzahl_dimensionen])
 geschwindigkeiten = np.zeros([anzahl_zeitschritte, anzahl_teilchen, anzahl_dimensionen])
 
 
@@ -96,12 +116,13 @@ geschwindigkeiten[0] = anfangsgeschwindigkeiten
 zeiten = np.array([schritt * groesse_zeitschritt for schritt in range(anzahl_zeitschritte)])
 for schritt in range(1, anzahl_zeitschritte):
     orte[schritt], geschwindigkeiten[schritt] = zeitschritt(orte[schritt-1], geschwindigkeiten[schritt-1], groesse_zeitschritt)
-    if (schritt+1)%100 == 0:
+    if (schritt+1)%1000 == 0:
         print(f"{schritt+1}/{anzahl_zeitschritte}\t:\t{round(schritt/anzahl_zeitschritte/10,3)*1000}%")
 
 header = "Teilchenpositionen"
 for teilchen in range(anzahl_teilchen):
     np.savetxt("Ort_Teilchen" + str(teilchen + 1) + ".txt", np.column_stack([zeiten, orte[:, teilchen, :]]), header=header)
     np.savetxt("Geschwindigkeit_Teilchen" + str(teilchen + 1) + ".txt", np.column_stack([geschwindigkeiten[:, teilchen, :]]), header=header)
+    if teilchen%10 == 0: print(f"s\t:\t{round((teilchen/anzahl_teilchen)*100,1)}%")
 
 print("\nFertig")
