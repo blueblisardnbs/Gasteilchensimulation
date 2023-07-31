db2ac89aba6f8a98da6ab15c2286186d31591edb
Niel Bodamer <60315174+blueblisardnbs@users.noreply.github.com>
2023-07-31 18:37:22 +0200

    Update simulation.py
    -Lennard-Jones eingeführt (Teilchen verhalten sich nicht mehr wie harte Kugeln)
    -Gesamtenergie

--- simulation.py
+++ simulation.py
@@ -1,10 +1,13 @@
 import numpy as np
 import scipy.spatial.distance as distance
+import math
+from stoß import harter_Stoß, wechselwirkungen
+from wand import wand
 
 groesse_zeitschritt = 0.005
 
 anzahl_zeitschritte = 10000
-anzahl_teilchen = 5000
+anzahl_teilchen = 100
 anzahl_dimensionen = 2
 
 m_1 = 1
@@ -12,7 +15,11 @@ m_2 = 1
 
 radius_teilchen = 0.05
 
-seitenlänge_Käfig = 100
+seitenlänge_Käfig = 10000
+
+sigma = 1
+
+ww = True # Wechselwirkungen oder harter Stoß
 
 np.savetxt("Konfiguration.txt", (anzahl_teilchen, anzahl_zeitschritte))
 
@@ -75,23 +82,57 @@ def stoß(x, v, teilchen1, teilchen2):
 def zeitschritt(x, v, dt):
 
     v_neu = np.copy(v)
+    '''
+    for d in range(anzahl_dimensionen):
+        for t in range(anzahl_teilchen):
+            if x[t, d] < 0 or x[t, d] > seitenlänge_Käfig:
+                pass
+                #print("bpoing")
+    '''
+    E_pot_w = 0
+    
+    for t in range(anzahl_teilchen):
+        #print(wand(x[t], v_neu[t], seitenlänge_Käfig, anzahl_dimensionen))
+        #print(v_neu[t])
+        v_neu[t] += wand(x[t], v_neu[t], seitenlänge_Käfig, anzahl_dimensionen)[0] * groesse_zeitschritt
+        E_pot_w += wand(x[t], v_neu[t], seitenlänge_Käfig, anzahl_dimensionen)[1]
+        #for d in range(anzahl_dimensionen):
+        #    v_neu[t, d] += wand(x[t], v_neu[t], seitenlänge_Käfig, anzahl_dimensionen)[d]
+    
+    '''
     for t in range(anzahl_teilchen):
         for d in range(anzahl_dimensionen):
-            if x[t, d] > seitenlänge_Käfig or x[t, d] < 0:
+            if x[t, d] > seitenlänge_Käfig or x[t, d] < 0:#0:
                 v_neu[t,d] *= -1
+    '''
                 
     distanzen = distance.pdist(x)
     q = distance.squareform(distanzen)
+    
+    
+    if not ww:
+        t1, t2 = np.where(q < 2*radius_teilchen)
+    
+        for tt1, tt2 in zip(t1, t2):
+            if tt1 < tt2:
+                v_neu[tt1, :], v_neu[tt2, :] = stoß(x, v, tt1, tt2)
+                
+        x_neu = x + v_neu * dt
+    
+        return x_neu, v_neu#, E_pot_w
+    else:
+        t1, t2 = np.where(q < 3* sigma) #200*radius_teilchen)
 
-    t1, t2 = np.where(q < 2*radius_teilchen)
-
-    for tt1, tt2 in zip(t1, t2):
-        if tt1 < tt2:
-            v_neu[tt1, :], v_neu[tt2, :] = stoß(x, v, tt1, tt2)
-            
-    x_neu = x + v_neu * dt
-
-    return x_neu, v_neu
+        Epot = 0
+        for tt1, tt2 in zip(t1, t2):
+            if tt1 < tt2:
+                v_neu[tt1, :], v_neu[tt2, :], Epot_tt = wechselwirkungen(x[tt1], x[tt2], orte[schritt-1, tt1], orte[schritt-1, tt2], v[tt1], v[tt2], groesse_zeitschritt, seitenlänge_Käfig)
+                #print("Boiiiiiinngg")
+                Epot += Epot_tt
+                
+        x_neu = x + v_neu * dt
+    
+        return x_neu, v_neu, Epot+E_pot_w
 
 anfangspositionen = generate_random_initial_pos(anzahl_teilchen,anzahl_dimensionen, radius_teilchen)
 
@@ -109,15 +150,19 @@ anfangsgeschwindigkeiten = 2*np.random.random((anzahl_teilchen, anzahl_dimension
 orte = np.zeros([anzahl_zeitschritte, anzahl_teilchen, anzahl_dimensionen])
 geschwindigkeiten = np.zeros([anzahl_zeitschritte, anzahl_teilchen, anzahl_dimensionen])
 
+E_pot = np.zeros(anzahl_zeitschritte)
 
-orte[0] = anfangspositionen
-geschwindigkeiten[0] = anfangsgeschwindigkeiten
+orte[0] = anfangspositionen#[[1,1],[1,9]]
+geschwindigkeiten[0] = anfangsgeschwindigkeiten#[[1,1],[1,-1]]
+
 
 zeiten = np.array([schritt * groesse_zeitschritt for schritt in range(anzahl_zeitschritte)])
 for schritt in range(1, anzahl_zeitschritte):
-    orte[schritt], geschwindigkeiten[schritt] = zeitschritt(orte[schritt-1], geschwindigkeiten[schritt-1], groesse_zeitschritt)
+    orte[schritt], geschwindigkeiten[schritt], E_pot_schritt = zeitschritt(orte[schritt-1], geschwindigkeiten[schritt-1], groesse_zeitschritt)
+    #print(E_pot_schritt)
     if (schritt+1)%1000 == 0:
         print(f"{schritt+1}/{anzahl_zeitschritte}\t:\t{round(schritt/anzahl_zeitschritte/10,3)*1000}%")
+    E_pot[schritt] = E_pot_schritt
 
 header = "Teilchenpositionen"
 for teilchen in range(anzahl_teilchen):
@@ -125,4 +170,10 @@ for teilchen in range(anzahl_teilchen):
     np.savetxt("Geschwindigkeit_Teilchen" + str(teilchen + 1) + ".txt", np.column_stack([geschwindigkeiten[:, teilchen, :]]), header=header)
     if teilchen%10 == 0: print(f"s\t:\t{round((teilchen/anzahl_teilchen)*100,1)}%")
 
+np.savetxt("E_pot.txt", np.column_stack([E_pot]), header=header)
+
+print(E_pot)
+    
 print("\nFertig")
+
+import plot
